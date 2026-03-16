e3e26254e4356015e216e065ca4ed4e0415045d9
Fabio Luiz Paranhos Borelli <fabioborelli@msn.com>
2026-03-15 21:42:53 -0300

    Implantação do Identity

--- QuarentenarioWeb/Program.cs
+++ QuarentenarioWeb/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using QuarentenarioWeb.Data;
 
@@ -9,15 +10,27 @@ namespace QuarentenarioWeb
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddRazorPages();
-
             var connectionString = builder.Configuration.GetConnectionString("QuarentanarioDB")
                 ?? throw new InvalidOperationException("Connection string"
                 + "'DefaultConnection' not found.");
 
             builder.Services.AddDbContext<QuarentenarioContext>(options =>
                 options.UseSqlServer(connectionString));
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            // Registro do Identity com suporte a Razor Pages (Default UI)
+            //builder.Services.AddDefaultIdentity(options => {
+            //    options.SignIn.RequireConfirmedAccount = false; // Ajuste conforme necessário
+            //})
+            //    .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
 
             var app = builder.Build();
 
@@ -33,6 +46,7 @@ namespace QuarentenarioWeb
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapStaticAssets();
