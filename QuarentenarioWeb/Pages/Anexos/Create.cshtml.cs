a27a3dba5a5ae27241d5098973ac71805aba3569
Fabio Luiz Paranhos Borelli <fabioborelli@msn.com>
2026-03-21 15:47:07 -0300

    Anexos, criar editar

--- QuarentenarioWeb/Pages/Anexos/Create.cshtml.cs
+++ QuarentenarioWeb/Pages/Anexos/Create.cshtml.cs
@@ -1,46 +1,102 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using QuarentenarioWeb.Data;
 using QuarentenarioWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace QuarentenarioWeb.Pages.Anexos
 {
     public class CreateModel : PageModel
     {
         private readonly QuarentenarioWeb.Data.QuarentenarioContext _context;
+        private readonly IWebHostEnvironment _environment;
 
-        public CreateModel(QuarentenarioWeb.Data.QuarentenarioContext context)
+        public CreateModel(QuarentenarioWeb.Data.QuarentenarioContext context, IWebHostEnvironment environment)
         {
             _context = context;
+            _environment = environment;
         }
 
         public IActionResult OnGet()
         {
-        ViewData["IdAnaliseDetalhe"] = new SelectList(_context.AnaliseDetalhes, "Id", "Id");
-        ViewData["IdAnalise"] = new SelectList(_context.Analises, "Id", "Id");
+            PopularControles();
             return Page();
         }
 
         [BindProperty]
         public Anexo Anexo { get; set; } = default!;
 
+        [BindProperty]
+        public IFormFile? Upload { get; set; }
+
         // For more information, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
+            // Validate file upload
+            if (Upload == null || Upload.Length == 0)
+            {
+                ModelState.AddModelError("Upload", "Selecione um arquivo para upload.");
+            }
+
+            ModelState.Remove($"{nameof(Anexo)}.{nameof(Anexo.NomeArmazenado)}");
+            ModelState.Remove($"{nameof(Anexo)}.{nameof(Anexo.NomeArquivo)}");
+            ModelState.Remove($"{nameof(Anexo)}.{nameof(Anexo.TipoConteudo)}");
+
             if (!ModelState.IsValid)
             {
+                PopularControles();
                 return Page();
             }
 
+            // populate Anexo properties from uploaded file
+            var originalFileName = Path.GetFileName(Upload!.FileName);
+            var storedFileName = Guid.NewGuid().ToString("N") + Path.GetExtension(originalFileName);
+
+            Anexo.NomeArquivo = originalFileName;
+            Anexo.NomeArmazenado = storedFileName;
+            Anexo.TipoConteudo = Upload.ContentType ?? string.Empty;
+
+            // Save file to App_Data/Anexos inside content root (not under wwwroot)
+            var storageDir = Path.Combine(_environment.ContentRootPath, "App_Data", "Anexos");
+            if (!Directory.Exists(storageDir))
+            {
+                Directory.CreateDirectory(storageDir);
+            }
+
+            var filePath = Path.Combine(storageDir, storedFileName);
+            using (var stream = System.IO.File.Create(filePath))
+            {
+                await Upload.CopyToAsync(stream);
+            }
+
             _context.Anexos.Add(Anexo);
-            await _context.SaveChangesAsync();
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                ModelState.AddModelError(string.Empty, "Não foi possível incluir o anexo. Deverá se escolhida uma análise ou uma análise detalhe.");
+                //+ (ex.InnerException?.Message ?? ex.Message));
+                PopularControles();
+                return Page();
+            }
 
             return RedirectToPage("./Index");
+        }
+
+        private void PopularControles()
+        {
+            ViewData["IdAnaliseDetalhe"] = new SelectList(_context.AnaliseDetalhes, "Id", "Descricao");
+            ViewData["IdAnalise"] = new SelectList(_context.Analises, "Id", "Descricao");
         }
     }
 }
--- QuarentenarioWeb/Pages/Anexos/Edit.cshtml.cs
+++ QuarentenarioWeb/Pages/Anexos/Edit.cshtml.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using QuarentenarioWeb.Data;
 using QuarentenarioWeb.Models;
 
@@ -14,14 +17,19 @@ namespace QuarentenarioWeb.Pages.Anexos
     public class EditModel : PageModel
     {
         private readonly QuarentenarioWeb.Data.QuarentenarioContext _context;
+        private readonly IWebHostEnvironment _environment;
 
-        public EditModel(QuarentenarioWeb.Data.QuarentenarioContext context)
+        public EditModel(QuarentenarioWeb.Data.QuarentenarioContext context, IWebHostEnvironment environment)
         {
             _context = context;
+            _environment = environment;
         }
 
         [BindProperty]
         public Anexo Anexo { get; set; } = default!;
+
+        [BindProperty]
+        public IFormFile? Upload { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -30,24 +38,75 @@ namespace QuarentenarioWeb.Pages.Anexos
                 return NotFound();
             }
 
-            var anexo =  await _context.Anexos.FirstOrDefaultAsync(m => m.Id == id);
+            var anexo = await _context.Anexos.FirstOrDefaultAsync(m => m.Id == id);
             if (anexo == null)
             {
                 return NotFound();
             }
             Anexo = anexo;
-           ViewData["IdAnaliseDetalhe"] = new SelectList(_context.AnaliseDetalhes, "Id", "Id");
-           ViewData["IdAnalise"] = new SelectList(_context.Analises, "Id", "Id");
+            PopularControles();
             return Page();
+        }
+
+        private void PopularControles()
+        {
+            ViewData["IdAnaliseDetalhe"] = new SelectList(_context.AnaliseDetalhes, "Id", "Descricao");
+            ViewData["IdAnalise"] = new SelectList(_context.Analises, "Id", "Descricao");
         }
 
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more information, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
+            ModelState.Remove($"{nameof(Anexo)}.{nameof(Anexo.NomeArmazenado)}");
+            ModelState.Remove($"{nameof(Anexo)}.{nameof(Anexo.NomeArquivo)}");
+            ModelState.Remove($"{nameof(Anexo)}.{nameof(Anexo.TipoConteudo)}");
+
             if (!ModelState.IsValid)
             {
+                PopularControles();
                 return Page();
+            }
+
+            var existing = await _context.Anexos.AsNoTracking().FirstOrDefaultAsync(a => a.Id == Anexo.Id);
+            if (existing == null)
+            {
+                return NotFound();
+            }
+
+            // If a new file was uploaded, replace stored file
+            if (Upload != null && Upload.Length > 0)
+            {
+                var originalFileName = Path.GetFileName(Upload.FileName);
+                var storedFileName = Guid.NewGuid().ToString("N") + Path.GetExtension(originalFileName);
+
+                // Save new file
+                var storageDir = Path.Combine(_environment.ContentRootPath, "App_Data", "Anexos");
+                if (!Directory.Exists(storageDir)) Directory.CreateDirectory(storageDir);
+                var newFilePath = Path.Combine(storageDir, storedFileName);
+                using (var stream = System.IO.File.Create(newFilePath))
+                {
+                    await Upload.CopyToAsync(stream);
+                }
+
+                // delete old file if exists
+                if (!string.IsNullOrEmpty(existing.NomeArmazenado))
+                {
+                    var oldPath = Path.Combine(storageDir, existing.NomeArmazenado);
+                    try { if (System.IO.File.Exists(oldPath)) System.IO.File.Delete(oldPath); } catch { }
+                }
+
+                // update Anexo fields
+                Anexo.NomeArquivo = originalFileName;
+                Anexo.NomeArmazenado = storedFileName;
+                Anexo.TipoConteudo = Upload.ContentType ?? string.Empty;
+            }
+            else
+            {
+                // keep previous file info
+                Anexo.NomeArquivo = existing.NomeArquivo;
+                Anexo.NomeArmazenado = existing.NomeArmazenado;
+                Anexo.TipoConteudo = existing.TipoConteudo;
             }
 
             _context.Attach(Anexo).State = EntityState.Modified;
@@ -66,6 +125,13 @@ namespace QuarentenarioWeb.Pages.Anexos
                 {
                     throw;
                 }
+            }
+            catch (DbUpdateException ex)
+            {
+                ModelState.AddModelError(string.Empty, "Não foi possível incluir o anexo. Deverá se escolhida uma análise ou uma análise detalhe.");
+                //+ (ex.InnerException?.Message ?? ex.Message));
+                PopularControles();
+                return Page();
             }
 
             return RedirectToPage("./Index");
