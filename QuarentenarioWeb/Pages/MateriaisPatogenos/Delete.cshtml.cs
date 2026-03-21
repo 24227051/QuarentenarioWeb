01615a1c49076ef74445d364886f966301ef45e0
Fabio Luiz Paranhos Borelli <fabioborelli@msn.com>
2026-03-21 11:09:31 -0300

    Material x Patógeno (incluir e remover associação)

--- QuarentenarioWeb/Pages/MateriaisPatogenos/Delete.cshtml.cs
+++ QuarentenarioWeb/Pages/MateriaisPatogenos/Delete.cshtml.cs
@@ -7,6 +7,19 @@ namespace QuarentenarioWeb.Pages.MateriaisPatogenos
 {
     public class DeleteModel : PageModel
     {
+        private readonly QuarentenarioWeb.Data.QuarentenarioContext _context;
+
+        public DeleteModel(QuarentenarioWeb.Data.QuarentenarioContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Patogeno Patogeno { get; set; } = default!;
+
+        [BindProperty]
+        public Material Material { get; set; } = default!;
+
         public async Task<IActionResult> OnGetAsync(int? idPatogeno, int? idMaterial)
         {
             if (idPatogeno == null || idMaterial == null)
@@ -14,16 +27,40 @@ namespace QuarentenarioWeb.Pages.MateriaisPatogenos
                 return NotFound();
             }
 
-            //var analisedetalhe = await _context.AnaliseDetalhes.FirstOrDefaultAsync(m => m.Id == id);
-
-            //if (analisedetalhe is not null)
-            //{
-            //    AnaliseDetalhe = analisedetalhe;
-
-            //    return Page();
-            //}
+            var material = await _context.Materials.FirstOrDefaultAsync(m => m.Id == idMaterial);
+            var patogeno = await _context.Patogenos.Where(p => p.Id == idPatogeno).FirstOrDefaultAsync();
+            if (material is not null && patogeno is not null)
+            {
+                Material = material;
+                Patogeno = patogeno;
+                return Page();
+            }
 
             return NotFound();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var idMaterial = Material?.Id;
+            var idPatogeno = Patogeno?.Id;
+
+            if (idMaterial == null || idPatogeno == null)
+                return NotFound();
+
+            var patogeno = await _context.Materials
+                .Include(m => m.IdPatogenos)
+                .Where(m => m.Id == idMaterial)
+                .SelectMany(m => m.IdPatogenos.Where(p => p.Id == idPatogeno))
+                .FirstOrDefaultAsync();
+
+            if (patogeno is not null)
+            {     
+                _context.Materials
+                    .Include(m => m.IdPatogenos).FirstOrDefault(m => m.Id == idMaterial)?.IdPatogenos.Remove(patogeno);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index", new { id = idMaterial });
         }
     }
 }
